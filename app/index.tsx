38b110b50cbd59c3312dddb7d84675a77d7c1064
shreylazy <5aprilshrey@gmail.com>
2025-10-24 19:57:24 +0530

    feat: update app configuration, enhance splash screen, and upgrade dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,11 @@ import {
   View,
   Text,
   StatusBar,
-  ImageBackground,
   Animated,
+  ImageBackground,
   Dimensions,
 } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { useRouter } from 'expo-router';
 
 const { width, height } = Dimensions.get('window');
@@ -32,17 +33,20 @@ export default function SplashScreen() {
   }, [router]);
 
   return (
-    <ImageBackground
-      source={require('../assets/images/splash.png')} // 👈 place your AI/futuristic art image here
-      style={{
-        flex: 1,
-        width,
-        height,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-      resizeMode='cover'
-    >
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('../assets/images/splash.png')} // 👈 place your AI/futuristic art image here
+        style={{
+          flex: 1,
+          width,
+          height,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        resizeMode='cover'
+      >
+      </ImageBackground>
+
       <StatusBar barStyle='light-content' />
 
       {/* Text Container with Blur */}
@@ -53,7 +57,38 @@ export default function SplashScreen() {
           alignItems: 'center',
         }}
       >
+        <LinearGradient
+          colors={['rgba(241, 241, 241, 0.88)', 'rgba(255, 255, 255, 1)']}
+          style={{
+            height: '200',
+            width: '100%',
+            position: 'absolute',
+            bottom: 0,
+            borderRadius: 20
+          }}
+        />
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: 'bold',
+            color: '#333',
+            marginBottom: 20,
+            fontFamily: 'Poppins-Bold',
+          }}
+        >
+          Chroma wallpapers
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            color: '#555',
+            marginBottom: 60,
+            fontFamily: 'Poppins-Regular',
+          }}
+        >
+          AI-generated wallpapers at your fingertips
+        </Text>
       </Animated.View>
-    </ImageBackground>
+    </View>
   );
 }
